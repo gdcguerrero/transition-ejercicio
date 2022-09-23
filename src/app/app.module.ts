a2b1dad0fd7f1b6ef15460a3d438c00384b31f73
Narbok <narbok29@gmail.com>
2022-09-22 20:57:02 -0500

    ejercicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransitionDirective } from './libs/directives/transition.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransitionDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/libs/directives/transition.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, Input, OnInit, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appTransition]'
+})
+export class TransitionDirective {
+
+  @Input('entrada') entrada: string ='';
+  
+  @HostListener('mouseenter') mouseE (){ //entra al elemeto cabia
+    this.element.nativeElement.style.transition = this.entrada;
+  }
+
+  constructor(private element: ElementRef) {    
+  }
+
+  ngOnInit(): void {
+  }
+}
